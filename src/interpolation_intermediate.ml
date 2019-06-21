d39402e04ea4815c4eed1e8c1ad106b59d3ddec3
Konstantin Romanov <kromanov1@bloomberg.net>
2019-06-21 20:28:27 +0000

    Remove unnecessary code.

--- src/interpolation_intermediate.ml
+++ src/interpolation_intermediate.ml
@@ -6,14 +6,14 @@ let parser_to_emitter (tokens : (P.token*Location.t) list) : E.token list =
     List.fold_left
         (fun (cur_fmt, acc) (token, loc) ->
             match token, cur_fmt with
-            | P.Format fmt, None -> 
+            | P.Format fmt, None ->
                     Some (fmt, loc), acc
-            | P.Expression e, curr_fmt -> 
+            | P.Expression e, curr_fmt ->
                     None, E.Expression ((e, loc), curr_fmt)::acc
-            | P.Variable v, curr_fmt -> 
+            | P.Variable v, curr_fmt ->
                     None, E.Variable ((v, loc), curr_fmt)::acc
             | _, Some (_, loc) -> raise (P.ParseStringError (loc.Location.loc_start, "Format is not followed by variable/expression. Missing %?"))
-            | P.String s, None -> 
+            | P.String s, None ->
                     None, E.String (s, loc)::acc
             | P.DollarChar, None ->
                     None, E.String ("$", loc)::acc
--- src/ppx_string_interpolation.ml
+++ src/ppx_string_interpolation.ml
@@ -6,9 +6,7 @@ let expand ~(loc:Location.t) ~(path:string) (str:string) : expression =
   let intermediate = Interpolation_intermediate.parser_to_emitter parsed in
   let ast = Interpolation_emitter.emit_ast intermediate in
   Pprintast.expression Format.std_formatter ast;
-  { pexp_desc = ast.pexp_desc;
-    pexp_loc = Lexing.{ loc_start = dummy_pos; loc_end = dummy_pos; loc_ghost = false};
-    pexp_attributes = []}
+  ast
 
 let extension = Extension.declare "string"
                     Extension.Context.Expression
@@ -16,20 +14,3 @@ let extension = Extension.declare "string"
 
 let () = Ppxlib.Driver.register_transformation "string_interpolation"
                               ~rules:[Context_free.Rule.extension extension]
-
-(*
-(* Now registering Value Format transformations -- remove later *)
-let expand ~(loc:Location.t) ~(path:string) (str:string) : expression =
-  let parsed = String_interpolation_parser_vf.Parser.parse_string str in
-  let ast = String_interpolation_emitter.generate true parsed in
-  { pexp_desc = ast.pexp_desc;
-    pexp_loc = Lexing.{ loc_start = dummy_pos; loc_end = dummy_pos; loc_ghost = false};
-    pexp_attributes = []}
-
-let extension = Extension.declare "stringVF"
-                    Extension.Context.Expression
-                    Ast_pattern.(single_expr_payload (estring __)) expand
-
-let () = Ppxlib.Driver.register_transformation "string_interpolationValueFormat"
-                              ~rules:[Context_free.Rule.extension extension]
-*)
