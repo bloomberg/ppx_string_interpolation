3ef082b30ed132a4bedf9336cabd7a5156027165
Konstantin Romanov <kromanov1@bloomberg.net>
2019-06-24 22:00:15 +0000

    Add examples from README to tests.

--- test/ppx_string_interpolation_test.ml
+++ test/ppx_string_interpolation_test.ml
@@ -1,6 +1,17 @@
-let%test _ = "HI" = "HI"
+(* Examples from README.md *)
+let%test _ =
+    let name = "world" in
+    [%string "Hello $name!"] = "Hello world!"
 
-let%test _ = [%string "Hello!6"] = "Hello!6"
+let%test _ =
+    let hello = "Hello" and world = "world" in
+    [%string {|$(hello ^ " " ^ world)!|}] = "Hello world!"
+
+let%test _ = let a = 1 and b = 1.0 in
+    [%string {|We know, that %d$a == %f$b is %b$(a = int_of_float b)!|}] = "We know, that 1 == 1.000000 is true!"
+
+(* Other tests *)
+let%test _ = [%string "Hello!"] = "Hello!"
 
 let%test _ = [%string {j|Hello!%d$(1)|j}] = "Hello!1"
 
@@ -9,9 +20,6 @@ let%test _ = "Hello!" ^ "1" = "Hello!1"
 let%test _ = [%string "Hello!%d$(1 + 2 + 3)"] = "Hello!6"
 
 let%test _ = let name = "Me!" in [%string "Hello, my name is $name! Good bye, $name."] = "Hello, my name is Me!! Good bye, Me!."
-
-let%test _ = let a = 1 and b = 1.0 in
-    [%string {|We know, that %d$a == %f$b is %b$(a = int_of_float b)!|}] = "We know, that 1 == 1.000000 is true!"
 
 let%test _ = [%string "Hello!"] = "Hello!"
 
